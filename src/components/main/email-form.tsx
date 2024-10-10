f69b986fb10187d542927127232f3842d51ff0e3
José Carlos Marquetti Junior <jose@apicedobrasil.com.br>
2024-10-10 14:39:03 -0300

    Atualização de layout responsivo seção video

--- src/components/main/email-form.tsx
+++ src/components/main/email-form.tsx
@@ -6,6 +6,14 @@ const FormContainer = styled.div`
     align-items: center;
     padding: 0 70px 70px 70px;
     border-radius: 30px;
+
+    @media (max-width: 768px) {
+        padding: 0 20px 50px 20px;
+    }
+
+    @media (max-width: 480px) {
+        padding: 0 10px 40px 10px;
+    }
 `;
 
 const Input = styled.input`
@@ -21,12 +29,19 @@ const Input = styled.input`
     ::placeholder {
         color: #fff!important;
     }
+
+    @media (max-width: 768px) {
+        width: 80%;
+        margin-bottom: 0px;
+        border-radius: 30px 0 0 30px;
+    }
 `;
+
 const Button = styled.button`
     padding: 15px 30px;
     margin-left: 10px;
     border: 2px solid #ffc107;
-    background-color: #ffc107;;
+    background-color: #ffc107;
     border-radius: 30px;
     color: #000;
     font-size: 16px;
@@ -34,6 +49,14 @@ const Button = styled.button`
 
     &:hover {
         background-color: #ffb300;
+    }
+
+    @media (max-width: 768px) {
+        width: 25%;
+        margin-left: 0;
+
+        border-radius: 0 30px 30px 0;
+        padding: 15px 2px;
     }
 `;
 
--- src/components/main/input-newslatter.tsx
+++ src/components/main/input-newslatter.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import FormEmail from './email-form'
+import FormEmail from './email-form';
 
 const FormContainer = styled.section`
-
     background-color: var(--button-color-primary);
     border-radius: 20px;
+    padding-bottom: 70px;
 
-    div{
+    div {
         max-width: 900px;
         margin: 0 auto;
+        padding: 0 20px;
     }
-    h2{
+
+    h2 {
         padding: 70px 70px 30px 70px;
         color: #fff;
         font-size: 28px;
         font-weight: 600;
         line-height: 39.2px;
         text-align: center;
+
+        @media (max-width: 768px) {
+            padding: 50px 20px 20px 20px;
+            font-size: 24px;
+            line-height: 34px;
+        }
+
+        @media (max-width: 480px) {
+            padding: 40px 15px 15px 15px;
+            font-size: 20px;
+            line-height: 30px;
+        }
     }
 `;
-
 
 const Newsletter: React.FC = () => {
     return (
         <FormContainer>
           <div>
             <h2>Be the first to know about the latest deals, receive new trending recipes & more!</h2>
-            <FormEmail/>
+            <FormEmail />
           </div>
         </FormContainer>
     );
